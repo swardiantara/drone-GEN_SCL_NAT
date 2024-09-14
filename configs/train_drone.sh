e4ad72734c5df948a13071d7956353d182470a61
swardiantara <swardyantara@gmail.com>
2024-09-14 15:09:10 +0700

    drone multiclass acos

---
+++ configs/train_drone.sh
@@ -0,0 +1,19 @@
+python3 source/gen_scl_nat_main.py \
+   --task gen_scl_nat \
+   --do_train \
+   --do_direct_eval \
+   --model_name_or_path t5-base \
+   --dataset acos_drone_binary \
+   --output_folder train_outputs \
+   --n_gpu 1 \
+   --train_batch_size 32 \
+   --eval_batch_size 32 \
+   --learning_rate 9e-5 \
+   --gradient_accumulation_steps 1 \
+   --num_train_epochs 45 \
+   --num_beams 5 \
+   --weight_decay 0.0 \
+   --seed 42 \
+   --cont_loss 0.05 \
+   --cont_temp 0.25 \
+   --model_prefix drone_output
---
+++ configs/train_drone_asqp.sh
@@ -0,0 +1,18 @@
+python3 source/gen_scl_nat_main.py \
+   --task asqp \
+   --do_train \
+   --do_direct_eval \
+   --dataset acos_drone_data \
+   --output_folder train_drone_asqp \
+   --n_gpu 1 \
+   --train_batch_size 16 \
+   --eval_batch_size 16 \
+   --learning_rate 9e-5 \
+   --gradient_accumulation_steps 1 \
+   --num_train_epochs 45 \
+   --num_beams 5 \
+   --weight_decay 0.0 \
+   --seed 42 \
+   --cont_loss 0.05 \
+   --cont_temp 0.25 \
+   --model_prefix drone_asqp
