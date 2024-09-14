a39d8c19fe3301b20571d0672277c7b6aed37385
swardiantara <swardyantara@gmail.com>
2024-09-14 16:21:22 +0700

    fix debug

--- source/gen_scl_nat_main.py
+++ source/gen_scl_nat_main.py
@@ -43,7 +43,7 @@ from eval_utils import compute_scores
 logger = logging.getLogger(__name__)
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-CUDA_LAUNCH_BLOCKING=1
+os.environ['CUDA_LAUNCH_BLOCKING'] = 1
 
 def init_args():
     parser = argparse.ArgumentParser()
