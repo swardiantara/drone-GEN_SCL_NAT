aaf67a872809bf38dec2e7bf3d42251d437397f7
swardiantara <swardyantara@gmail.com>
2024-09-14 18:01:30 +0700

    fix duplicate entry in optimizer

--- source/gen_scl_nat_main.py
+++ source/gen_scl_nat_main.py
@@ -318,14 +318,6 @@ class T5FineTuner(pl.LightningModule):
                 "weight_decay": 0.0,
             },
             {
-                "params": [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
-                "weight_decay": self.hparams.weight_decay,
-            },
-            {
-                "params": [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)],
-                "weight_decay": 0.0,
-            },
-            {
                 "params": [p for n, p in cont_model.named_parameters() if not any(nd in n for nd in no_decay)],
                 "weight_decay": self.hparams.weight_decay,
             },
