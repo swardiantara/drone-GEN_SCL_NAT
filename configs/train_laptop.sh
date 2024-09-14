d4e695fa5efec7042e390d293e4dec1a32a74390
swardiantara <swardyantara@gmail.com>
2024-09-14 17:20:06 +0700

    reduce batch_size

--- configs/train_laptop.sh
+++ configs/train_laptop.sh
@@ -8,8 +8,8 @@ python source/gen_scl_nat_main.py \
    --model_name_or_path t5-base \
    --output_folder inference_outputs \
    --n_gpu 1 \
-   --train_batch_size 32 \
-   --eval_batch_size 32 \
+   --train_batch_size 16 \
+   --eval_batch_size 16 \
    --learning_rate 9e-5 \
    --gradient_accumulation_steps 1 \
    --num_train_epochs 2 \
