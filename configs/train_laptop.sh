ac81eaad1b8fe265a66c58518730be33098847b1
swardiantara <swardyantara@gmail.com>
2024-09-15 23:59:57 +0700

    try laptop with 45 epochs

--- configs/train_laptop.sh
+++ configs/train_laptop.sh
@@ -8,11 +8,11 @@ python source/gen_scl_nat_main.py \
    --model_name_or_path t5-base \
    --output_folder train_outputs \
    --n_gpu 1 \
-   --train_batch_size 16 \
-   --eval_batch_size 16 \
+   --train_batch_size 32 \
+   --eval_batch_size 32 \
    --learning_rate 9e-5 \
    --gradient_accumulation_steps 1 \
-   --num_train_epochs 2 \
+   --num_train_epochs 45 \
    --num_beams 5 \
    --weight_decay 0.0 \
    --seed 123 \
