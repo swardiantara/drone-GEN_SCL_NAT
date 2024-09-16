62d725cdad241f3e327dcab86effc2d0939c7780
swardiantara <swardyantara@gmail.com>
2024-09-16 23:18:32 +0700

    back using AutoModel

--- source/gen_scl_nat_main.py
+++ source/gen_scl_nat_main.py
@@ -473,10 +473,10 @@ if __name__ == '__main__':
         tfm_model = T5ForConditionalGeneration.from_pretrained(args.model_name_or_path)
 
         if args.embedding == 'sbert':
-            # embedding_model = AutoModel.from_pretrained("sentence-transformers/all-mpnet-base-v2").to(device)
-            embedding_model = SentenceTransformer("all-mpnet-base-v2").to(device)
-            embedding_model.tokenizer = tokenizer
-            tfm_model.set_input_embeddings(embedding_model._first_module())
+            embedding_model = AutoModel.from_pretrained("sentence-transformers/all-mpnet-base-v2").to(device)
+            embedding_model.resize_token_embeddings(len(tokenizer))
+            # embedding_model = SentenceTransformer("all-mpnet-base-v2").to(device)
+            tfm_model.set_input_embeddings(embedding_model)
         else:
             tfm_model.resize_token_embeddings(len(tokenizer))
 
