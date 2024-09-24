22cf6ca160bdabecb81a0a5209c81c79fe676f5d
swardiantara <swardyantara@gmail.com>
2024-09-24 22:28:12 +0700

    try to fix save_pretrained to save the .bin file

--- source/train_embedding.py
+++ source/train_embedding.py
@@ -157,6 +157,7 @@ def main():
         word_embedding_model = models.Transformer(args.model_name_or_path, do_lower_case=False)
         pooling_model = models.Pooling(word_embedding_dimension=word_embedding_model.get_word_embedding_dimension(), pooling_mode='cls')
         model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+        model.modules
 
     # Load your dataset
     dataframe, quad_dataframe = construct_dataset(args)
@@ -185,7 +186,7 @@ def main():
         output_path=args.output_dir
     )
 
-    bert_model = next(model.modules)
+    bert_model = next(model.modules())
     bert_model.auto_model.save_pretrained(save_directory=args.output_dir, safe_serialization=False, variant=args.scenario)
 
     return 0
